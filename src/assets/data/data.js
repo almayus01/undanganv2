366a33b4dc6f658d28b58e29ef8e4bf94421a65d
almayus01 <34200342+almayus01@users.noreply.github.com>
2024-12-19 11:14:05 +0700

    Add files via upload

---
+++ src/assets/data/data.js
@@ -0,0 +1,125 @@
+export const data = {
+  bride: {
+    L: {
+      id: 1,
+      name: "Andi Gumilar",
+      child: "Putra Pertama",
+      father: "Ipan Mulyana",
+      mother: "Dian Ida Farida",
+      image: "src/assets/images/cowo.png",
+    },
+    P: {
+      id: 2,
+      name: "Mutiara Amalia Maulidini, A.Md.Farm",
+      child: "Putri ke Dua",
+      father: "Asep Tahyan Yohana",
+      mother: "Yayah Rokayah",
+      image: "src/assets/images/cewe.png",
+    },
+
+    couple: "src/assets/images/Screenshot 2024-12-13 170022.png",
+  },
+
+  time: {
+    marriage: {
+      year: "2025",
+      month: "Februari",
+      date: "02",
+      day: "Minggu",
+      hours: {
+        start: "08.00",
+        finish: "Selesai",
+      },
+    },
+    reception: {
+      year: "2025",
+      month: "Februari",
+      date: "02",
+      day: "Minggu",
+      hours: {
+        start: "11.00",
+        finish: "Selesai",
+      },
+    },
+    address: "Dusun Cibelah RT 03 RW  01 Desa Sidamulya, Kec. Cisaga Kab. Ciamis",
+  },
+
+  link: {
+    map: "https://maps.app.goo.gl/sAENSSLxj6xuQSo97",
+  },
+
+  galeri: [
+    {
+      id: 1,
+      image: "src/assets/images/1.png",
+    },
+    {
+      id: 2,
+      image: "src/assets/images/2.png",
+    },
+    {
+      id: 3,
+      image: "src/assets/images/3.png",
+    },
+    {
+      id: 4,
+      image: "src/assets/images/4.png",
+    },
+    {
+      id: 5,
+      image: "src/assets/images/5.png",
+    },
+  ],
+
+  bank: [
+    {
+      id: 1,
+      name: "Andi Gumilar",
+      icon: "./src/assets/images/bri.png",
+      rekening: "405201021496532",
+    },
+    {
+      id: 2,
+      name: "Mutiara Amalia Maulani",
+      icon: "./src/assets/images/bri.png",
+      rekening: "402001023238535",
+    },
+  ],
+
+  audio: "src/assets/audio/backsound.mp3",
+
+  api: "https://script.google.com/macros/s/AKfycbzdB9306Q6Wlm4kqdlb8m8xcTsHVlPamVkI1RMO6psP9ouL4XPNpWPJcYzyxdlB5Ixx6w/exec",
+
+  navbar: [
+    {
+      id: 1,
+      teks: "Home",
+      icon: "bx bxs-home-heart",
+      path: "#home",
+    },
+    {
+      id: 2,
+      teks: "Mempelai",
+      icon: "bx bxs-group",
+      path: "#bride",
+    },
+    {
+      id: 3,
+      teks: "Tanggal",
+      icon: "bx bxs-calendar-check",
+      path: "#time",
+    },
+    {
+      id: 4,
+      teks: "Galeri",
+      icon: "bx bxs-photo-album",
+      path: "#galeri",
+    },
+    {
+      id: 5,
+      teks: "Ucapan",
+      icon: "bx bxs-message-rounded-dots",
+      path: "#wishas",
+    },
+  ],
+};
